8ea479d3f491cb39b64e756947d28926f9112386
chenaoh <andresfel1304@gmail.com>
2023-10-19 18:17:22 -0500

    Se agrega el Proyecto

---
+++ # Función para realizar la suma.py
@@ -0,0 +1,46 @@
+# Función para realizar la suma
+def suma(a, b):
+    return a + b
+
+# Función para realizar la resta
+def resta(a, b):
+    return a - b
+
+# Función para realizar la multiplicación
+def multiplicacion(a, b):
+    return a * b
+
+# Función para realizar la división
+def division(a, b):
+    if b == 0:
+        return "No se puede dividir por cero"
+    return a / b
+
+# Menú de la calculadora
+while True:
+    print("Opciones:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Salir")
+
+    opcion = input("Elige una opción (1/2/3/4/5): ")
+
+    if opcion == '5':
+        break
+
+    if opcion in ('1', '2', '3', '4'):
+        num1 = float(input("Ingresa el primer número: "))
+        num2 = float(input("Ingresa el segundo número: "))
+
+        if opcion == '1':
+            print("Resultado:", suma(num1, num2))
+        elif opcion == '2':
+            print("Resultado:", resta(num1, num2))
+        elif opcion == '3':
+            print("Resultado:", multiplicacion(num1, num2))
+        elif opcion == '4':
+            print("Resultado:", division(num1, num2))
+    else:
+        print("Opción no válida. Por favor, elige una opción válida.")
